743ad9648b98edcacbe685b742d920934f4547d0
JandersonBrito <j.a.brito@live.com>
2016-10-10 03:20:31 -0300

    Primeiro commit

---
+++ Assets/Projeto/Scripit/MovimentacaoPersonagem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class MovimentacaoPersonagem : MonoBehaviour {
+
+
+    public float velocidadeMov;
+    public float velocidadeRot;
+    
+	// Use this for initialization
+	void Start () {
+
+        velocidadeMov = 4f;
+        velocidadeRot = 1.2f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("Vertical"))
+        {
+            transform.Rotate(0, 20 * velocidadeRot, 0);
+
+        }
+
+
+
+    }
+}
